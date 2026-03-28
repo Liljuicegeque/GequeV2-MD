443d09e8d2d67da0aa307952c2c38e103a313ba1
Liljuicegeque <mhlangezanagershom9@gmail.com>
2026-03-28 11:27:14 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'GEQUE V2-MD',
+  author: '‎',
+  botName: "GEQUE V2 MD",
+  botOwner: 'LILJUICEGEQUE', // Your name
+  ownerNumber: '27630300636', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.9",
+  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
+};
+
+module.exports = settings;
